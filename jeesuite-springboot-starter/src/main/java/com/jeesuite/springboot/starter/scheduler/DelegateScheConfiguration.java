182680ca973ba417e9f4015762e98cd7b23036eb
vakinge <vakinge@gmail.com>
2018-10-22 21:39:53 +0800

    定时任务启动兼容官方默认

--- jeesuite-springboot-starter/src/main/java/com/jeesuite/springboot/starter/scheduler/DelegateScheConfiguration.java
+++ jeesuite-springboot-starter/src/main/java/com/jeesuite/springboot/starter/scheduler/DelegateScheConfiguration.java
@@ -5,8 +5,10 @@ package com.jeesuite.springboot.starter.scheduler;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -26,6 +28,7 @@ import com.jeesuite.scheduler.registry.ZkJobRegistry;
 @Configuration
 @EnableConfigurationProperties(SchedulerProperties.class)
 @ConditionalOnClass(SchedulerFactoryBeanWrapper.class)
+@AutoConfigureAfter(QuartzAutoConfiguration.class)
 @ConditionalOnProperty(name="jeesuite.task.groupName")
 public class DelegateScheConfiguration {
 
