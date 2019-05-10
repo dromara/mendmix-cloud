4e39c7276f967c304834650d5417d1bbd8bbbde2
vakinge <vakinge@gmail.com>
2019-05-10 20:57:56 +0800

    增加工具类

---
+++ jeesuite-common/src/main/java/com/jeesuite/common/util/GzipUtils.java
@@ -0,0 +1,106 @@
+package com.jeesuite.common.util;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
+public class GzipUtils {
+
+	public static final String GZIP_ENCODE_UTF_8 = "UTF-8";
+
+	public static final String GZIP_ENCODE_ISO_8859_1 = "ISO-8859-1";
+
+	/**
+	 * 字符串压缩为GZIP字节数组
+	 * 
+	 * @param str
+	 * @return
+	 */
+	public static byte[] compress(String str) {
+		return compress(str, GZIP_ENCODE_UTF_8);
+	}
+
+	/**
+	 * 字符串压缩为GZIP字节数组
+	 * 
+	 * @param str
+	 * @param encoding
+	 * @return
+	 */
+	public static byte[] compress(String str, String encoding) {
+		if (str == null || str.length() == 0) {
+			return null;
+		}
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		GZIPOutputStream gzip;
+		try {
+			gzip = new GZIPOutputStream(out);
+			gzip.write(str.getBytes(encoding));
+			gzip.close();
+		} catch (IOException e) {
+		}
+		return out.toByteArray();
+	}
+
+	/**
+	 * GZIP解压缩
+	 * 
+	 * @param bytes
+	 * @return
+	 */
+	public static byte[] uncompress(byte[] bytes) {
+		if (bytes == null || bytes.length == 0) {
+			return null;
+		}
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		ByteArrayInputStream in = new ByteArrayInputStream(bytes);
+		try {
+			GZIPInputStream ungzip = new GZIPInputStream(in);
+			byte[] buffer = new byte[256];
+			int n;
+			while ((n = ungzip.read(buffer)) >= 0) {
+				out.write(buffer, 0, n);
+			}
+		} catch (IOException e) {
+		}
+
+		return out.toByteArray();
+	}
+
+	/**
+	 * 
+	 * @param bytes
+	 * @return
+	 */
+	public static String uncompressToString(byte[] bytes) {
+		return uncompressToString(bytes, GZIP_ENCODE_UTF_8);
+	}
+
+	/**
+	 * 
+	 * @param bytes
+	 * @param encoding
+	 * @return
+	 */
+	public static String uncompressToString(byte[] bytes, String encoding) {
+		if (bytes == null || bytes.length == 0) {
+			return null;
+		}
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		ByteArrayInputStream in = new ByteArrayInputStream(bytes);
+		try {
+			GZIPInputStream ungzip = new GZIPInputStream(in);
+			byte[] buffer = new byte[256];
+			int n;
+			while ((n = ungzip.read(buffer)) >= 0) {
+				out.write(buffer, 0, n);
+			}
+			return out.toString(encoding);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
