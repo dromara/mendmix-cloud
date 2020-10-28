0d68bf4299c3b5faf54ac2286678114ccd9cbdd0
vakinge <vakinge@gmail.com>
2020-10-28 18:23:00 +0800

    数据权限支持

---
+++ jeesuite-mybatis/src/main/java/com/jeesuite/mybatis/plugin/datalog/DataChangeLogHandler.java
@@ -0,0 +1,92 @@
+package com.jeesuite.mybatis.plugin.datalog;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.plugin.Invocation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.jeesuite.mybatis.core.BaseEntity;
+import com.jeesuite.mybatis.core.InterceptorHandler;
+import com.jeesuite.mybatis.parser.EntityInfo;
+import com.jeesuite.mybatis.parser.MybatisMapperParser;
+import com.jeesuite.mybatis.plugin.JeesuiteMybatisInterceptor;
+
+/**
+ * 数据变更日志mybatis层拦截处理器
+ * <br>
+ * Class Name   : DataChangeLogHandler
+ *
+ * @author jiangwei
+ * @version 1.0.0
+ * @date 2019年10月28日
+ */
+public class DataChangeLogHandler implements InterceptorHandler {
+
+	private final static Logger logger = LoggerFactory.getLogger("om.jeesuite.mybatis.plugin.datalog");
+	
+	private static ThreadLocal<List<String>> entityClassNameHolder = new ThreadLocal<>();
+	private static String[] methodNames = new String[]{"insert","insertSelective","selectByPrimaryKey","deleteByPrimaryKey","updateByPrimaryKey","updateByPrimaryKeySelective"};
+	
+	public static void set(Class<? extends BaseEntity>[] entityClass){
+		List<String> names = new ArrayList<String>(entityClass.length);
+        for (Class<? extends BaseEntity> clazz : entityClass) {
+        	EntityInfo entityInfo = MybatisMapperParser.getEntityInfoByEntityName(clazz.getName());
+        	names.add(entityInfo.getMapperClass().getName());
+		}
+        entityClassNameHolder.set(names);
+	}
+	
+    public static void unset(){
+    	if(entityClassNameHolder.get() == null)return;
+    	entityClassNameHolder.get().clear();
+    	entityClassNameHolder.remove();
+	}
+
+	@Override
+	public void start(JeesuiteMybatisInterceptor context) {}
+
+	@Override
+	public Object onInterceptor(Invocation invocation) throws Throwable {
+		return null;
+	}
+
+	@Override
+	public void onFinished(Invocation invocation, Object result) {
+		if(entityClassNameHolder.get() == null || entityClassNameHolder.get().isEmpty())return;
+		Object[] args = invocation.getArgs();
+		MappedStatement mt = (MappedStatement)args[0]; 
+		
+		String mapperClassName = mt.getId().substring(0,mt.getId().lastIndexOf("."));
+		if(!entityClassNameHolder.get().contains(mapperClassName))return;
+		EntityInfo entityInfo = MybatisMapperParser.getEntityInfoByMapper(mapperClassName);
+		if(entityInfo == null)return;
+		try {
+			System.out.println(mt.getId());
+			if(mt.getId().endsWith(methodNames[0]) || mt.getId().endsWith(methodNames[1])){
+				//BehaviorLogContextHoler.onAddEntity(entityInfo.getEntityClass(), ((BaseEntity)args[1]).getId());
+			}else if(mt.getId().endsWith(methodNames[2])){
+				if(result != null && !((List)result).isEmpty()){					
+					//BehaviorLogContextHoler.onUpdateEntityBefore((BaseEntity)((List)result).get(0));
+				}
+			}else if(mt.getId().endsWith(methodNames[3])){
+	        	//BehaviorLogContextHoler.onDeleteEntity(entityInfo.getEntityClass(), args[1]);
+			}else if(mt.getId().endsWith(methodNames[4]) || mt.getId().endsWith(methodNames[5])){
+				//BehaviorLogContextHoler.onUpdateEntityAfter((BaseEntity)args[1]);
+			}
+		} catch (Exception e) {
+			logger.warn("datachange_interceptor_error for["+mt.getId()+"]",e);
+		}
+	}
+
+	@Override
+	public int interceptorOrder() {
+		return 9;
+	}
+	
+	@Override
+	public void close() {}
+
+}
---
+++ jeesuite-mybatis/src/main/java/com/jeesuite/mybatis/plugin/dataprofile/DataProfileHandler.java
@@ -0,0 +1,220 @@
+package com.jeesuite.mybatis.plugin.dataprofile;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.apache.ibatis.cache.CacheKey;
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.SqlCommandType;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.jeesuite.common.util.ResourceUtils;
+import com.jeesuite.mybatis.MybatisRuntimeContext;
+import com.jeesuite.mybatis.core.InterceptorHandler;
+import com.jeesuite.mybatis.crud.helper.ColumnMapper;
+import com.jeesuite.mybatis.crud.helper.EntityHelper;
+import com.jeesuite.mybatis.crud.helper.EntityMapper;
+import com.jeesuite.mybatis.parser.EntityInfo;
+import com.jeesuite.mybatis.parser.MybatisMapperParser;
+import com.jeesuite.mybatis.plugin.JeesuiteMybatisInterceptor;
+
+import net.sf.jsqlparser.JSQLParserException;
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.StringValue;
+import net.sf.jsqlparser.expression.operators.conditional.AndExpression;
+import net.sf.jsqlparser.expression.operators.relational.EqualsTo;
+import net.sf.jsqlparser.expression.operators.relational.ExpressionList;
+import net.sf.jsqlparser.expression.operators.relational.InExpression;
+import net.sf.jsqlparser.parser.CCJSqlParserUtil;
+import net.sf.jsqlparser.schema.Column;
+import net.sf.jsqlparser.schema.Table;
+import net.sf.jsqlparser.statement.select.Join;
+import net.sf.jsqlparser.statement.select.PlainSelect;
+import net.sf.jsqlparser.statement.select.Select;
+
+/**
+ * 数据权限mybatis层拦截处理器 <br>
+ * Class Name : DataProfileHandler
+ *
+ * @author jiangwei
+ * @version 1.0.0
+ * @date 2019年10月28日
+ */
+public class DataProfileHandler implements InterceptorHandler {
+
+	private final static Logger logger = LoggerFactory.getLogger("com.jeesuite.mybatis.plugin.dataprofile");
+    
+	public static final String NAME = "dataProfile";
+	
+	private List<String> excludeMapperIds;
+	private Map<String, List<ColumnMapper>> tableColumnMappings = new HashMap<>();
+	private Map<String, List<ColumnMapper>> dataProfileMappings = new HashMap<>();
+	
+	@Override
+	public void start(JeesuiteMybatisInterceptor context) {
+		List<String> fieldNames = Arrays.asList(ResourceUtils.getAndValidateProperty("jeesuite.mybatis.dataProfile.fieldNames").split(",|;"));
+		String[] excludeMapperIdArray = org.springframework.util.StringUtils.tokenizeToStringArray(ResourceUtils.getProperty("jeesuite.mybatis.dataProfile.excludeMapperIds",""), ConfigurableApplicationContext.CONFIG_LOCATION_DELIMITERS);
+		excludeMapperIds = Arrays.asList(excludeMapperIdArray);
+		
+		List<EntityInfo> entityInfos = MybatisMapperParser.getEntityInfos(context.getGroupName());
+		
+		List<ColumnMapper> columns;
+		for (EntityInfo entityInfo : entityInfos) {
+			EntityMapper entityMapper = EntityHelper.getEntityMapper(entityInfo.getEntityClass());
+			tableColumnMappings.put(entityMapper.getTableMapper().getName().toLowerCase(), new ArrayList<>(entityMapper.getColumnsMapper()));
+			for (ColumnMapper columnMapper : entityMapper.getColumnsMapper()) {
+				if(fieldNames.contains(columnMapper.getProperty())){
+					columns = dataProfileMappings.get(entityInfo.getMapperClass().getName());
+					if(columns == null){
+						columns = new ArrayList<>(fieldNames.size());
+						dataProfileMappings.put(entityInfo.getMapperClass().getName(), columns);
+					}
+					columns.add(columnMapper);
+				}
+			}
+		}
+		
+		logger.info("dataProfileMappings >> {}",dataProfileMappings);
+	}
+
+	@Override
+	public Object onInterceptor(Invocation invocation) throws Throwable {
+		final Executor executor = (Executor) invocation.getTarget();
+		final Object[] args = invocation.getArgs();
+		final MappedStatement mappedStatement = (MappedStatement) args[0];
+
+		if (!mappedStatement.getSqlCommandType().equals(SqlCommandType.SELECT))
+			return null;
+		
+		if(excludeMapperIds.contains(mappedStatement.getId()))return null;
+
+		Map<String, String[]> dataMappings = MybatisRuntimeContext.getDataProfileMappings();
+		if(dataMappings == null)return null;
+		
+		String mapperClassName = mappedStatement.getId().substring(0, mappedStatement.getId().lastIndexOf("."));
+		if(!dataProfileMappings.containsKey(mapperClassName))return null;
+		
+		final ResultHandler<?> resultHandler = (ResultHandler<?>) args[3];
+		final Object parameter = args[1];
+
+		BoundSql boundSql;
+		if (args.length == 4) {
+			boundSql = mappedStatement.getBoundSql(parameter);
+		} else {
+			boundSql = (BoundSql) args[5];
+		}
+
+		CacheKey countKey = executor.createCacheKey(mappedStatement, parameter, RowBounds.DEFAULT, boundSql);
+
+		String orignSql = StringUtils.replace(boundSql.getSql(), ";$", StringUtils.EMPTY);
+
+		String newSql = buildDataProfileSql(orignSql,new ArrayList<>(dataProfileMappings.get(mapperClassName)),dataMappings);
+
+		BoundSql newBoundSql = new BoundSql(mappedStatement.getConfiguration(), newSql,
+				boundSql.getParameterMappings(), parameter);
+
+		List<?> resultList = executor.query(mappedStatement, parameter, RowBounds.DEFAULT, resultHandler, countKey,
+				newBoundSql);
+
+		return resultList;
+	}
+
+	/**
+	 * @param orignSql
+	 * @param columnDefineList
+	 * @param dataMappings
+	 * @return
+	 */
+	private String buildDataProfileSql(String orignSql, List<ColumnMapper> columnDefineList,Map<String, String[]> dataMapping) {
+		Select select = null;
+		try {
+			select = (Select) CCJSqlParserUtil.parse(orignSql);
+		} catch (JSQLParserException e) {
+			logger.error("rebuildDataProfileSql_ERROR",e);
+			throw new RuntimeException("sql解析错误");
+		}
+		
+		PlainSelect selectBody = (PlainSelect) select.getSelectBody();
+		Table table = (Table) selectBody.getFromItem();
+		
+		Iterator<ColumnMapper> iterator = columnDefineList.iterator();
+		Expression newExpression = null;
+		ColumnMapper item;
+		while(iterator.hasNext()){
+			item =iterator.next();
+			if(!dataMapping.containsKey(item.getProperty()))continue;
+			newExpression = appendDataProfileCondition(table, selectBody.getWhere(), item.getColumn(),dataMapping.get(item.getProperty()));
+			selectBody.setWhere(newExpression);
+			//主表已经处理的条件，join表不在处理
+			iterator.remove();
+		}
+		
+		//JOIN 
+		List<Join> joins = selectBody.getJoins();
+		if(joins != null && !columnDefineList.isEmpty()){
+			List<ColumnMapper> columns;
+			for (Join join : joins) {
+				table = (Table) join.getRightItem();
+				columns = tableColumnMappings.get(table.getName().toLowerCase());
+				if(columns == null)continue;
+				for (ColumnMapper defineColumn : columnDefineList) {
+					if(!columns.contains(defineColumn))continue;
+					if(!dataMapping.containsKey(defineColumn.getProperty()))continue;
+					newExpression = appendDataProfileCondition(table, join.getOnExpression(), defineColumn.getColumn(),dataMapping.get(defineColumn.getProperty()));
+					join.setOnExpression(newExpression);
+				}
+				
+			}
+		}
+		//
+		String newSql = selectBody.toString();
+		
+		return newSql;
+	}
+	
+	private static Expression appendDataProfileCondition(Table table,Expression orginExpression,String columnName,String[] values){
+		Expression newExpression = null;
+		Column column = new Column(table, columnName);
+		if (values.length == 1) {
+			EqualsTo equalsTo = new EqualsTo();
+			equalsTo.setLeftExpression(column);
+			equalsTo.setRightExpression(new StringValue(values[0]));
+			newExpression = orginExpression == null ? equalsTo : new AndExpression(orginExpression, equalsTo);
+		} else {
+			ExpressionList expressionList = new ExpressionList(new ArrayList<>(values.length));
+			for (String value : values) {
+				expressionList.getExpressions().add(new StringValue(value));
+			}
+			InExpression inExpression = new InExpression(column, expressionList);
+			newExpression = orginExpression == null ? inExpression : new AndExpression(orginExpression,inExpression);
+		}
+		
+		return newExpression;
+	}
+
+	@Override
+	public void onFinished(Invocation invocation, Object result) {
+	}
+
+	@Override
+	public int interceptorOrder() {
+		return 2;
+	}
+
+	@Override
+	public void close() {
+	}
+
+}
